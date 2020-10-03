dbac5251ee527f7db7664b7934a132aa56421b5a
Danicho23 <henry.brito4040@gmail.com>
2020-10-03 18:01:36 -0500

    quita campo calificacion

--- src/main/java/com/shoplite/model/Proveedor.java
+++ src/main/java/com/shoplite/model/Proveedor.java
@@ -1,12 +1,16 @@
 package com.shoplite.model;
 
+import lombok.Data;
 import org.springframework.data.annotation.Id;
-
+import org.springframework.data.mongodb.core.mapping.Document;
+@Data
+//crea en Mongodb la collection
+@Document
 public class Proveedor {
 @Id
 private String id;
-private String cedula;
+private String ruc;
+private String nombre;
 private String fecha_entrega;
-private String calificacion;
 
 }
